d97f251e656277538c082ad5b08666135a241b3c
avenmia <mavend2@gmail.com>
2023-10-24 19:16:04 -1000

    Adding card link components to organize links

--- app/page.tsx
+++ app/page.tsx
@@ -2,35 +2,28 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Link from "next/link";
+import CardLink from "./components/CardLink";
 
 const Home: React.FC = () => {
   return (
     <>
       <Navbar />
       <main className="bg-cover bg-no-repeat flex min-h-screen flex-col items-center justify-between px-4 ">
-        <div
+        {/* <div
           className="absolute top-0 left-0 w-full h-full -z-10"
           // style={{
           //   backgroundImage: `url('/assets/image (4).jpg')`,
           //   backgroundSize: "cover",
           //   backgroundRepeat: "no-repeat",
           // }}
-        ></div>
-          <Link className="w-1 h-1" href="/report">
-            <h2 className="text-white">Report Debris</h2>
-          </Link>
-          <Link className="w-1 h-1" href="/remove">
-            <h2 className="text-white">Remove Debris</h2> 
-          </Link>
-          <Link className="w-1 h-1" href="/transport">
-            <h2 className="text-white">Transport Debris</h2> 
-          </Link>
-          <Link className="w-1 h-1" href="/store">
-            <h2 className="text-white">Store Debris</h2> 
-          </Link>
-          <Link className="w-1 h-1" href="/results">
-            <h2 className="text-white">View Debris Reports</h2> 
-          </Link>
+        ></div> */}
+          <div className="flex flex-row flex-wrap w-2/3 justify-around">
+            <CardLink link="report" title="Report Debris" />
+            <CardLink link="remove" title="Remove Debris" />
+            <CardLink link="transport" title="Transport Debris" />
+            <CardLink link="store" title="Store Debris" />
+            <CardLink link="results" title="View Debris Reports" />
+          </div>
       </main>
       <Footer />
     </>
