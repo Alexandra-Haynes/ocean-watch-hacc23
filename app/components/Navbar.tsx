00d9d1596e4717847a97e32d72873b6a4eb0f3c2
Alex <sandamunteanua@yahoo.com>
2023-10-27 10:29:06 -1000

    add logo

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar: React.FC = () => {
     <nav className="bg-slate-800 p-4">
       <div className="container mx-auto flex justify-between items-center">
         <a href="#" className="text-white font-bold text-2xl">
-          Logo
+          <img src='/assets/logo.png' className="h-12"></img>
         </a>
         <ul className="flex space-x-4">
           <li>
