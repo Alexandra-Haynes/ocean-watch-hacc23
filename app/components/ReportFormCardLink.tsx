579aa6740b9adf7fe96d9dea987ea5144cf3f736
Alex <sandamunteanua@yahoo.com>
2023-10-27 10:29:27 -1000

    style report card link

---
+++ app/components/ReportFormCardLink.tsx
@@ -0,0 +1,63 @@
+import Link from "next/link";
+import { BiSolidPhoneCall } from "react-icons/bi";
+import { LuBadgeAlert } from "react-icons/lu";
+
+
+export default function ReportFormCardLink() {
+  return (
+    <>
+      <Link
+        className="rounded lg:overflow-hidden  mt-12
+         h-[1100px] lg:h-[1200px] w-[700px] bg-white 
+         shadow-2xl flex flex-col items-center justify-start
+         relative hover:shadow-2xl transition duration-300 ease-out"
+        href={`/report`}
+      >
+        <div className="flex flex-col items-center justify-start mt-[5rem]">
+          <h2
+            className="text-center font-bold text-3xl mb-6 
+          font-mono uppercase"
+          >
+            Found Marine Debris?
+          </h2>
+          <h3
+            className="max-w-[300px] text-xl text-center mb-6 font-semibold
+           text-slate-700"
+          >
+            Report It Here, and We'll Take Care of the Removal
+          </h3>
+        </div>
+        <div
+          className=" text-xl text-white mt-4
+           w-1/2 h-[4rem] uppercase rounded-full
+          flex flex-col items-center justify-center shadow-lg
+          custom-background hover:translate-y-1 hover:shadow-2xl transition duration-500 ease-in-out
+          "
+        >
+          Report debris title
+        </div>
+
+        <div
+          className="h-[240px] w-2/3 bg-cyan-300/10 rounded-md shadow-lg
+flex flex-col items-center justify-center mt-8 gap-2 py-4"
+        >
+          <LuBadgeAlert className="text-6xl text-orange-400" />
+          <p className="font-semibold px-6 text-center text-lg">
+            Are there marine animals entangled in the debris?
+          </p>
+          <div className="h-[1px] w-1/2 bg-orange-400 my-1"></div>
+          <p>Call NOAA immediately at </p>
+          <p className="text-xl font-bold flex flex-row items-center justify-center gap-2">
+            <BiSolidPhoneCall />
+            1-888-256-9840
+          </p>
+          <p className="text-[.8rem]">24/7 Emergency Hotline</p>
+        </div>
+        <div
+          className="h-[400px] lg:h-[600px] w-full absolute bottom-0 bg-slate-900
+        bg-[url('/assets/sealion.png')] bg-cover bg-no-repeat bg-bottom"
+        ></div>
+      </Link>
+    </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -3,30 +3,33 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Link from "next/link";
 import CardLink from "./components/CardLink";
+import ReportFormCardLink from "./components/ReportFormCardLink";
 
 const Home: React.FC = () => {
   return (
-    <>
+    <section className="max-h-screen overflow-y-hidden">
       <Navbar />
-      <main className="bg-cover bg-no-repeat flex min-h-screen flex-col items-center justify-between px-4 ">
-        {/* <div
-          className="absolute top-0 left-0 w-full h-full -z-10"
-          // style={{
-          //   backgroundImage: `url('/assets/image (4).jpg')`,
-          //   backgroundSize: "cover",
-          //   backgroundRepeat: "no-repeat",
-          // }}
-        ></div> */}
-          <div className="flex flex-row flex-wrap w-2/3 justify-around">
-            <CardLink link="report" title="Report Debris" />
-            <CardLink link="remove" title="Remove Debris" />
-            <CardLink link="transport" title="Transport Debris" />
-            <CardLink link="store" title="Store Debris" />
-            <CardLink link="results" title="View Debris Reports" />
+      <main className="bg-cover bg-no-repeat flex min-h-screen 
+      flex-col items-center justify-between px-4">
+        <div
+          className="absolute top-0 left-0 w-full h-full -z-10
+          custom-background"
+        >
+          <div className="custom-clip-path h-screen w-screen bg-white/10">
+            {/* Your page content goes here */}
           </div>
+        </div>
+        <div className="flex flex-row flex-wrap w-2/3 justify-around">
+          <ReportFormCardLink />
+          {/* <CardLink link="report" title="Report Debris" /> */}
+          {/* <CardLink link="remove" title="Remove Debris" /> */}
+          {/* <CardLink link="transport" title="Transport Debris" /> */}
+          {/* <CardLink link="store" title="Store Debris" /> */}
+          {/* <CardLink link="results" title="View Debris Reports" /> */}
+        </div>
       </main>
-      <Footer />
-    </>
+      {/* <Footer /> */}
+    </section>
   );
 };
 
