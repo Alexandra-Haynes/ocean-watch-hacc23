26fe8c9c92d63abcf5dd35aea6aad635c81d27b2
avenmia <mavend2@gmail.com>
2023-11-08 14:54:25 -1000

    Migrating more removal form fields (#24)

--- app/remove/RemovalForm.tsx
+++ app/remove/RemovalForm.tsx
@@ -58,17 +58,66 @@ export default function RemovalForm() {
 
   return (
     <section>
-      <form onSubmit={handleSubmit}>
-        <h2>Removal Form</h2>
-        <div>
-          <label htmlFor="environmentalDamage">Environmental Damage:</label>
-          <textarea
-            id="environmentalDamage"
-            name="environmentalDamage"
-            value={formData.environmentalDamage}
+      <form onSubmit={handleSubmit}
+      className="flex flex-col items-start justify-center gap-2
+      border-slate-500/30 border-2 rounded-md shadow-2xl p-6  md:p-8 bg-white/90 text-black w-fit"
+      >
+        <h2 className=" font-medium text-2xl self-center">Removal Form</h2>
+        <div className="flex flex-row items-start justify-between md:gap-12 ">
+        <div className="mr-8">
+        <div className="form-group flex flex-row items-center gap-4 w-fit">
+          <label htmlFor="date">Detection Date:</label>
+          <input
+            className="text-black"
+            type="date"
+            id="detectionDate"
+            name="detectionDate"
+            value={formData.detectionDate}
             onChange={handleChange}
-            className="w-full lg:max-w-[700px] h-[200px] p-2 bg-white rounded-sm text-black "
           />
+        </div>
+        <div className="form-group flex flex-row items-center gap-4 w-fit">
+          <label htmlFor="text">Detection Location:</label>
+          <input
+            className="text-black"
+            type="text"
+            id="detectionLocation"
+            name="detectionLocation"
+            value={formData.detectionLocation}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group flex flex-row items-center gap-4 w-fit">
+          <label htmlFor="text">Debris Type:</label>
+          <input
+            className="text-black"
+            type="text"
+            id="debrisType"
+            name="debrisType"
+            value={formData.debrisType}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group flex flex-row items-center gap-4 w-fit">
+          <label htmlFor="text">Debris Approx Size:</label>
+          <input
+            className="text-black"
+            type="text"
+            id="debrisApproxSize"
+            name="debrisApproxSize"
+            value={formData.debrisApproxSize}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group flex flex-row items-center gap-4 w-fit">
+          <label htmlFor="text">Environmental Damage:</label>
+            <textarea
+              id="environmentalDamage"
+              name="environmentalDamage"
+              value={formData.environmentalDamage}
+              onChange={handleChange}
+              className="w-full lg:max-w-[700px] h-[200px] p-2 bg-white rounded-sm text-black "
+            />
         </div>
         <button
           className="bg-green-600 text-white px-4 py-2 rounded-md
@@ -77,6 +126,8 @@ export default function RemovalForm() {
         >
           Submit Report
         </button>
+        </div>
+        </div>
       </form>
     </section>
   );
