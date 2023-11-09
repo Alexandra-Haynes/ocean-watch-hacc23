dda71a0f4e30784706b7ba73b160964fce18f601
avenmia <mavend2@gmail.com>
2023-11-08 14:49:23 -1000

    Formatting

--- app/components/HeroText.tsx
+++ app/components/HeroText.tsx
@@ -3,16 +3,23 @@ import React from "react";
 const HeroText = () => {
   return (
     <div className="md:ml-6 flex flex-col items-start justify-center gap-2 lg:gap-6">
-      <img src="./assets/hpu logo.png" className="max-w-[160px] xl:max-w-[400px] lg:max-w-[300px] 
-      hidden lg:block"/>
+      <img
+        src="./assets/hpu logo.png"
+        className="max-w-[160px] xl:max-w-[400px] lg:max-w-[300px] 
+      hidden lg:block"
+      />
       <div className="h-[1px] w-[300px] bg-blue-300/20 hidden lg:block my-4"></div>
       <h1 className="text-2xl lg:text-4xl xl:text-6xl font-extrabold text-white text-center lg:text-left">
-        <p className="lg:hidden uppercase mb-2 mt-6">Hawai's Pacific University</p>
+        <p className="lg:hidden uppercase mb-2 mt-6">
+          Hawai's Pacific University
+        </p>
         Center for Marine Debris Research
       </h1>
 
-      <p className="text-blue-200 lg:text-xl xl:text-2xl max-w-[300px] lg:max-w-[500px] font-semibold
-      hidden lg:block">
+      <p
+        className="text-blue-200 lg:text-xl xl:text-2xl max-w-[300px] lg:max-w-[500px] font-semibold
+      hidden lg:block"
+      >
         World's premier center for pioneering plastic pollution research
       </p>
     </div>
--- app/remove/page.tsx
+++ app/remove/page.tsx
@@ -2,9 +2,9 @@ import RemovalForm from "./RemovalForm";
 
 export default function RemovePage() {
   return (
-  <>
-    <h1>Removal Page</h1>
-    <RemovalForm />
-  </>
-  )
+    <>
+      <h1>Removal Page</h1>
+      <RemovalForm />
+    </>
+  );
 }
