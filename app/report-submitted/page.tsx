863ec22ecdd9a72f338e0706e50bc234c9a0e737
Hooobot <hoobot0@gmail.com>
2023-11-11 15:01:21 -1000

    Pretty page for thank you

--- app/report-submitted/page.tsx
+++ app/report-submitted/page.tsx
@@ -1,26 +1,28 @@
-import HeroText from "../components/HeroText";
-import Navbar from "../components/Navbar";
+import Link from "next/link";
 
 export default function ReportSubmitted() {
   return (
     <section className="max-h-screen overflow-y-hidden">
-      <Navbar />
-      <main
-        className="bg-cover bg-no-repeat flex min-h-screen 
-          flex-col items-center justify-between px-4"
-      >
-        <div
-          className="absolute top-0 left-0 w-full h-full -z-10
-              custom-background"
-        >
+      <main className="bg-cover bg-no-repeat flex flex-col items-center justify-center min-h-screen px-4 relative">
+        <div className="absolute top-0 left-0 w-full h-full -z-10 custom-background">
           <div className="custom-clip-path h-screen w-screen bg-white/10"></div>
         </div>
-        <h1
-          className="text-white-700 text-xl font-semibold uppercase
-        transition-all ease-in-out justify-center"
-        >
-          Thank you!
-        </h1>
+
+        <div className="text-center p-6 bg-white bg-opacity-80 rounded-xl shadow-lg">
+          <h1 className="text-4xl font-bold text-gray-800 mb-6">Thank You!</h1>
+          <p className="text-lg text-gray-600 mb-6">
+            Your report has been submitted successfully.
+          </p>
+          <Link
+            href="/"
+            className=" text-xl text-white mt-4
+             h-[4rem] uppercase rounded-full
+           flex flex-col items-center justify-center shadow-lg
+           custom-background hover:translate-y-1 hover:shadow-2xl transition duration-500 ease-in-out"
+          >
+            Return to Home
+          </Link>
+        </div>
       </main>
     </section>
   );
